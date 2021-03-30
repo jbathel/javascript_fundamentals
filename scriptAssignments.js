bfbc1c03bfe6390f4d94254544650ec39e3c912d
Jessica Bathel <jessica.bathel@gmail.com>
2021-03-30 16:14:39 -0700

    Updating challenges and assignments

--- scriptAssignments.js
+++ scriptAssignments.js
@@ -79,3 +79,50 @@ console.log(description2);
 // 1. If your country's population is greater than 33 million, log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
 
 // 2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original
+
+//////////////////////////////////////////////
+// LECTURE: Type Conversion and Coercion
+//////////////////////////////////////////////
+
+// 1. Predict the result of these 5 operations without executing them:
+'9' - '5'; // 9
+// Type converting to number then doing math === 4
+'19' - '13' + '17'; // 1917
+// Type converting to number to perform 19 - 13 === 6 then concatenating 17 === 617
+'19' - '13' + 17; // 1917
+// Type converting then performing math, 17 is a number therefore added (19 - 13 = 6 + 17 === 23)
+'123' < 57; // false
+// Is not type converting therefore a string is not comparable to number
+5 + 6 + '4' + 9 - 4 - 2; // 1143
+// Type conversion to perform math operations, then concatenating '4', then performing math operations
+
+// 2. Execute the operations to check if you were right
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+//////////////////////////////////////////////
+// LECTURE: Equality Operators: == vs. ===
+//////////////////////////////////////////////
+
+// 1. Declare a variable 'numNeighbours' based on a prompt input like this:
+// prompt('How many neighbour countries does your country have?');
+let numNeighbours = Number(prompt('How many neighbor countries does your country have?'));
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!'(use loose equality == for now)
+if (numNeighbours === 1) {
+  console.log('Only 1 border!');
+}
+// 3. Use an else-if block to log 'More than 1 border.' in case 'numNeighbours' is greater than 1
+else if (numNeighbours > 1) {
+  console.log('More than 1 border.');
+}
+// 4. Use an else block to log 'No borders' (this block will be executed when 'numNeighbours' is 0 or any other value)
+else {
+  console.log('No borders.');
+}
+// 5. Test the code with different values of 'numNeighbours', including 1 and 0.
+// 6. Change == to ===, and test the code again, with the same values of 'numNeighbours'. Notice what happens when there is exactly 1 border! Why is this happening?
+// 7. Finally, convert 'numNeighbours' to a number, and watch what happens now when you input 1
+// 8. Reflect on why we should use the === operator and type conversion in this situation
--- scriptChallenges.js
+++ scriptChallenges.js
@@ -49,3 +49,54 @@ if (markHigherBMI) {
 } else {
   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+//////////////////////////////////////////////
+// Coding Challenge #3
+//////////////////////////////////////////////
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123 
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+const dolphinsAverage = (97 + 112 + 101) / 3;
+const koalasAverage = (109 + 95 + 80) / 3;
+
+console.log(dolphinsAverage, koalasAverage);
+
+// 2. Compare the team's average scores to determine the winner of the competition,and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score)
+if (dolphinsAverage > koalasAverage) {
+  console.log('Dolphins win!');
+} else if (dolphinsAverage < koalasAverage) {
+  console.log('Koalas win!');
+} else if (dolphinsAverage === koalasAverage) {
+  console.log('Both teams win.');
+} else {
+  console.log('No one wins.');
+}
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and at the same time a score of at least 100 points.
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+  console.log('Dolphins win!');
+} else if (dolphinsAverage < koalasAverage && koalasAverage >= 100) {
+  console.log('Koalas win!');
+} else if (dolphinsAverage === koalasAverage) {
+  console.log('Both teams win.');
+} else {
+  console.log('It is a draw.');
+}
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy
+if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+  console.log('Dolphins win!');
+} else if (dolphinsAverage < koalasAverage && koalasAverage >= 100) {
+  console.log('Koalas win!');
+} else if (dolphinsAverage === koalasAverage && dolphinsAverage >= 100) {
+  console.log('It is a draw.');
+} else {
+  console.log('No one wins.');
+}
